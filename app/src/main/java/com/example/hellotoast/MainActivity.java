7f116343d1bd90d5491f3fb2b9a6cffd1d392042
aadarsh111 <aadarsh.rauniyar11@gmail.com>
2022-04-05 19:56:06 +0545

    HelloToastChallange

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(hellotoastchallenge.R.layout.activity_main);
         mShowCount=(TextView)findViewById(R.id.show_count);
     }
 
