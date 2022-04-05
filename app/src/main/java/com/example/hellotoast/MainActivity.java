2f3f79d3e1471f61bb59b7af48671d01809f25b6
aadarsh111 <aadarsh.rauniyar11@gmail.com>
2022-04-05 20:15:17 +0545

    Layout Editor Coding Challenge

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -8,13 +8,14 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
+
     private int mCount=0;
     private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(com.example.layoutEditorCodingChallenge1.R.layout.activity_main);
         mShowCount=(TextView)findViewById(R.id.show_count);
     }
 
